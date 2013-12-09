2bdda313a125b2369de69fd5b8dda49be437095a
willmendesneto <willmendesneto@gmail.com>
2013-12-08 22:33:52 -0300

    Modifications in send mail form for error return

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,61 @@
     $('#sidebar-wrapper').toggleClass('active');
   });
 
+  /**
+   * Active scroll to top of target element
+   * @param  {[type]} scrollHeight height to scroll EX: activeScrollTop( $('div').offset )
+   * @return {void}
+   */
+  function activeScrollTop( scrollHeight ) {
+      $('html, body').animate({ scrollTop: scrollHeight }, 'fast');
+  }
+
+  /**
+   * Active scroll to top of target element
+   * @param  {[type]} scrollHeight height to scroll EX: activeScrollTop( $('div').offset )
+   * @return {void}
+   */
+  function activeCloseAlertMessage( ) {
+      $('.close').on('click', function(){
+          $(this).parents('.send-mail-message').slideUp('fast').html('');
+      });
+  }
+
+  //  FORM VALIDATION SEND EMAIL
+  $('#send-mail').submit( function(event){
+    event.preventDefault();
+    var send_mail_items = $('#send-mail').serializeArray(),
+        $send_mail_message = $('.send-mail-message'),
+        el = {},
+        message = []
+    ;
+    $.each( send_mail_items, function(i, field) {
+      el[field.name] = field.value;
+    });
+    if (el.nome === '' || el.nome === 'Seu nome (obrigatório)') {
+      message.push('Nome é obrigatório');
+    }
+    if (el.email === '' || el.email === 'Seu nome (obrigatório)') {
+      message.push('Email é obrigatório');
+    }
+    //  Return message for user
+    if( message.length !== 0) {
+      $send_mail_message.html('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><p>' + message.join('<br/>') + '</p></div>' )
+                        .slideDown('fast');
+      activeScrollTop($send_mail_message.offset().top - 100 );
+      //  Activate alert close click
+      activeCloseAlertMessage();
+      return;
+    }
+
+    $.post('save_email.php', el, function(data) {
+      $send_mail_message.html(data);
+      activeScrollTop($send_mail_message.offset().top - 100);
+      //  Activate alert close click
+      activeCloseAlertMessage();
+    });
+  })
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') || location.hostname === this.hostname) {
 
---
+++ app/save_email.php
@@ -0,0 +1,25 @@
+<?php
+
+date_default_timezone_set('America/Recife');
+
+$filename = md5(date("Y-m-d-G-i-s.U")) . '.json';
+$stringReturn = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><p>Ops! Ocorreu um erro ao cadastrar seu email. Tente novamente mais tarde =).</p></div>';
+if ( (isset( $_POST['nome'] && !empty($_POST['nome'])) && (isset($_POST['email']) && !empty($_POST['email'])) ) {
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $hora = date('r');
+
+  $arr = array (
+    'nome' => $nome,
+    'email' => $email,
+    'hora' => $hora
+  );
+  $json = json_encode($arr);
+
+  $fh = fopen($filename,'w+');
+  fwrite($fh, $json);
+  fclose($fh);
+
+  $stringReturn = '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button><p>Obrigado! Em breve você receberá nossas novidades.</p></div>';
+}
+die($stringReturn);
