726bb84ea6069258f0f595a74d1c3808f4cd11b6
willmendesneto <willmendesneto@gmail.com>
2013-12-30 13:29:05 -0300

    Add "location" content and new bower component "gmaps.js"

--- app/js/main.js
+++ app/js/main.js
@@ -96,4 +96,138 @@
       }
     }
   });
+
+  /**
+   * Location
+   */
+  var map,
+      mapOpts = {}
+  ;
+  /**
+   * Draw route
+   * @param  {[type]} opts [description]
+   * @return {[type]}      [description]
+   */
+  var drawRouteInMaps = function(opts) {
+    map.drawRoute({
+      origin: [opts.origin.lat , opts.origin.lng ],
+      destination: [opts.destination.lat, opts.destination.lng],
+      travelMode: 'driving',
+      strokeColor: '#131540',
+      strokeOpacity: 0.6,
+      strokeWeight: 6
+    });
+  };
+
+
+  GMaps.geolocate({
+    success: function(position) {
+      mapOpts.origin = {
+        lat: position.coords.latitude.toFixed(6),
+        lng: position.coords.longitude.toFixed(6)
+      };
+    },
+    error: function(error) {
+      /*  Add Salvador Location*/
+      mapOpts.origin = {
+        lat: -12.971606,
+        lng: -38.501587
+      };
+    },
+    not_supported: function() {
+      alert("Your browser does not support geolocation");
+    },
+    always: function() {
+      /* Init Gmaps class*/
+      map = new GMaps({
+        div: '#contact',
+        lat: mapOpts.origin.lat,
+        lng: mapOpts.origin.lng,
+        zoomControl : true,
+        zoomControlOpt: {
+            style : 'SMALL',
+            position: 'TOP_LEFT'
+        }
+      });
+
+      map.addMarker({
+        lat:  mapOpts.origin.lat,
+        lng:  mapOpts.origin.lng,
+        color: 'blue',
+        title: 'Você está aqui ',
+        infoWindow: {
+          content: '<p>Você está aqui </p>'
+        },
+        verticalAlign: 'top',
+        horizontalAlign: 'center'
+      });
+
+      /* Set user location for center map*/
+      map.setCenter(mapOpts.origin.lat, mapOpts.origin.lng);
+
+      mapOpts.destination = {
+        lat: -12.961174,
+        lng: -38.432032,
+        icon: '/img/pin.png',
+        title: 'LinguÁgil 2014 - UNIFACS.'
+      };
+
+      map.addMarker({
+        lat:  mapOpts.destination.lat,
+        lng:  mapOpts.destination.lng,
+        title:  mapOpts.destination.title,
+        icon: mapOpts.destination.icon,
+        infoWindow: {
+          content: '<p>' + mapOpts.destination.title + '</p>' +
+                  '<p><a id="draw-destination-route">Como chegar</a></p>'
+        },
+      });
+
+      var $document = $(document);
+
+      $document.on('click', '#draw-destination-route', function(e) {
+        e.preventDefault();
+        var opts = {
+            origin: mapOpts.origin,
+            destination : mapOpts.destination
+          }
+        ;
+        drawRouteInMaps(opts);
+      });
+
+      map.addControl({
+        position: 'top_right',
+        style: {
+          margin: '5px',
+          padding: '1px 6px',
+          border: 'solid 1px #717B87',
+          background: '#fff'
+        },
+        content: 'Minha localização',
+        events: {
+          click: function(){
+            map.setCenter(mapOpts.origin.lat, mapOpts.origin.lng);
+          },
+        }
+      });
+
+      map.addControl({
+        position: 'top_right',
+        style: {
+          margin: '5px',
+          padding: '1px 6px',
+          border: 'solid 1px #717B87',
+          background: '#fff'
+        },
+        content: 'Local do evento',
+        events: {
+          click: function(){
+            map.setCenter(mapOpts.destination.lat, mapOpts.destination.lng);
+          },
+        }
+      });
+
+      map.fitZoom();
+    }
+  });
 })(jQuery);
